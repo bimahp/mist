aa609711626737a3e958edea5588a630fe0cb94a
bimahp <me@bimahp.com>
2016-12-02 01:40:28 +0700

    Add uuid and sequelize

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', function(req, res, next) {
+  console.log('register')
+  res.json({text: 'banana'})
+})
+
 module.exports = router;
